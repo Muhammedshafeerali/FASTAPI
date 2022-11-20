1d449f8742578659ba42608426ec869165114ebf
Muhammedshafeerali <shafeeralihp@gmail.com>
2022-11-20 16:26:14 +0530

    fastapi learning-2 path parameter and query parameter pydantic model

--- main.py
+++ main.py
@@ -1,31 +1,47 @@
 from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import Optional
+import uvicorn
 
 
-app=FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello":"World"}
+myapp=FastAPI()
 
-@app.get("/item/{item_id}")
+class Blog(BaseModel):
+    title: str
+    body: str
+    Published: Optional[bool]
+
+ 
+@myapp.get("/blog")
+def read_root(limit:int=10,published:bool=True,sort:Optional[str]=None):
+
+    return {"data":f"limit value {limit} and pulished {published}and sor{sort}"}
+
+@myapp.post("/blog")
+def create_blog(request:Blog):
+    return{"data":f"Blog creted with title {request.title} "}
+
+@myapp.get("/item/list")
+def item_lst():
+    return {"data":"list data presentiong"}
+ 
+@myapp.get("/item/{item_id}")
 def read_item(item_id:int,q:Union[str,None]=None):
     return {"item_id":item_id,"q":q}
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
+# @myapp.put("/item/{item_id}")
+# def update_item(item_id:int,item: Item):
+#     return {"item_name": item.name, "item_id": item_id}
 
-@app.put("/item/{item_id}")
-def update_item(item_id:int,item: Item):
-    return {"item_name": item.name, "item_id": item_id}
-
-    
+if __name__=="__main__":
+    uvicorn.run(myapp,host='127.0.0.1',port=9000)
 
 
 
 
 
 
+
+ 
